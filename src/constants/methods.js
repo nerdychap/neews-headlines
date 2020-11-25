97c9d7cb7078c7db6af2b9c4d5658877ec88d6ae
siyabonga <siyabonga.hlengwa@mirumagency.com>
2020-11-25 22:41:20 +0200

    adding date rollback to a month ago

--- src/constants/methods.js
+++ src/constants/methods.js
@@ -1,7 +1,13 @@
-const getDate = () => {
+const getCurrentDate = () => {
     const currentLocaleDateString = new Date().toLocaleDateString();
     const [day, month, year] = currentLocaleDateString.split('/');
     return [year, month, day].join('-');
 }
 
-export default getDate;
+export const getPastDate = () => {
+    const monthAgo = new Date().setDate(new Date().getDate() - 29);
+    const [day, month, year] = new Date(monthAgo).toLocaleDateString().split('/');
+    return [year, month, day].join('-');
+}
+
+export default getCurrentDate;
