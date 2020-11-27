f1bec865f845bc7d57f0d556d813bd00f2f391a0
siyabonga <siyabonga.hlengwa@mirumagency.com>
2020-11-27 16:41:50 +0200

    adding cross origin header

--- src/context/NewsContextProvider.js
+++ src/context/NewsContextProvider.js
@@ -23,7 +23,11 @@ const NewsContextProvider = ({ children }) => {
     const querySubmit = (event) => {
         event.preventDefault();
         setIsLoading(true)
-        Axios.get(`https://newsapi.org/v2/everything?q=${query}&from=${date}&to=${date}&sortBy=popularity&apiKey=f3fe839d93d44c96830015d70b6b29a1`)
+        Axios.get(`https://newsapi.org/v2/everything?q=${query}&from=${date}&to=${date}&sortBy=popularity&apiKey=f3fe839d93d44c96830015d70b6b29a1`, {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
             .then(results => {
                 setNews(results.data.articles);
                 setIsLoading(false);
@@ -38,7 +42,11 @@ const NewsContextProvider = ({ children }) => {
 
     useEffect(() => {
 
-        Axios.get(`https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=f3fe839d93d44c96830015d70b6b29a1`)
+        Axios.get(`https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=f3fe839d93d44c96830015d70b6b29a1`, {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
             .then(results => {
                 setNews(results.data.articles);
                 setIsLoading(false);
